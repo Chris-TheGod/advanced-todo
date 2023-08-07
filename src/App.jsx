0cc1f8f12c36cd85416a550f3a2e46a6302534d1
Kristjan Lääne <xonproject@gmail.com>
2023-08-07 18:21:39 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useReducer, useState } from 'react'
+import './styles.css'
+import { TodoItem } from './TodoItem'
+import { NewTodoForm } from './NewTodoForm'
+
+const LOCAL_STORAGE_KEY = 'TODOS'
+const ACTIONS = {
+  ADD: 'ADD',
+  UPDATE: 'UPDATE',
+  TOGGLE: 'TOGGLE',
+  DELETE: 'DELETE',
+}
+
+function reducer(todos, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD:
+      return [
+        ...todos,
+        { name: payload.name, completed: false, id: crypto.randomUUID() },
+      ]
+    case ACTIONS.TOGGLE:
+      return todos.map((todo) => {
+        if (todo.id === payload.id)
+          return { ...todo, completed: payload.completed }
+
+        return todo
+      })
+    case ACTIONS.DELETE:
+      return todos.filter((todo) => todo.id !== payload.id)
+
+    default:
+      throw new Error(`No action found for ${type}.`)
+  }
+}
+
+function App() {
+  const [todos, dispatch] = useReducer(reducer, [], (initialValue) => {
+    const value = localStorage.getItem(LOCAL_STORAGE_KEY)
+    if (value == null) return initialValue
+
+    return JSON.parse(value)
+  })
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function addNewTodo(name) {
+    dispatch({ type: ACTIONS.ADD, payload: { name } })
+  }
+
+  function toggleTodo(todoId, completed) {
+    dispatch({ type: ACTIONS.TOGGLE, payload: { id: todoId, completed } })
+  }
+
+  function deleteTodo(todoId) {
+    dispatch({ type: ACTIONS.DELETE, payload: { id: todoId } })
+  }
+
+  return (
+    <>
+      <ul id='list'>
+        {todos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              toggleTodo={toggleTodo}
+              deleteTodo={deleteTodo}
+            />
+          )
+        })}
+      </ul>
+
+      <NewTodoForm addNewTodo={addNewTodo} />
+    </>
+  )
+}
+
+export default App
